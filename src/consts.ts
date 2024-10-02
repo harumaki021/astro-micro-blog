5877a570b59a19b905520e6f6117139de8de58fd
harumaki021 <harusamerain@gmail.com>
2024-10-02 13:23:54 +0900

    update Metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,40 +1,40 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "harumakizaemon.net",
+  DESCRIPTION: "harumaki021のWebサイト",
+  EMAIL: "test@example.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "harumaki021のWebサイトです",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  DESCRIPTION: "harumaki021のブログです。VRなど興味のあることについて書いています",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+    "harumaki021がこれまで作ったりしたものたちの置き場です",
 };
 
 export const SOCIALS: Socials = [
   {
+    NAME: "Misskey",
+    HREF: "https://mi.harumakizaemon.net/@harumaki021",
+  },
+  {
     NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheeks",
+    HREF: "https://twitter.com/hrmkzaemon_vr",
   },
   {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
-  },
-  {
-    NAME: "LinkedIn",
-    HREF: "https://www.linkedin.com/in/trevortylerlee",
-  },
+    HREF: "https://github.com/harumaki021",
+  }
 ];
