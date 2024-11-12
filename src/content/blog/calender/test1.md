a9e7a5d312193bec3b3aae79fcb8f13b4094ce83
harumaki021 <harusamerain@gmail.com>
2024-11-12 15:58:02 +0900

    うごかない

---
+++ src/content/blog/calender/test1.md
@@ -0,0 +1,9 @@
+---
+title: "2024-12-12のテスト"
+description: 2024-12-12説明
+date: 2024-12-12
+draft: false
+tags: ["blog"]
+---
+
+## h2
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,13 @@ export default {
         sans: ["ヒラギノ丸ゴ ProN","Hiragino Maru Gothic ProN","Hiragino Kaku Gothic ProN","ヒラギノ角ゴ ProN W3","BIZ UDPGothic","Helvetica Neue","sans-serif"],
         mono: ["M PLUS 1 Code Variable","sans-serif", ...defaultTheme.fontFamily.mono],
       },
+      colors: {
+        'green-500': '#48bb78',
+        'gray-300': '#e2e8f0',
+      },
+      spacing: {
+        '12': '3rem',
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
