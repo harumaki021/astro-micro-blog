838d51f9771fe990f68357f00f80fb2ce921111f
harumaki021 <harusamerain@gmail.com>
2024-12-13 11:28:05 +0900

    Fix: readingTimeMinutes 日本語を考慮した計算に修正

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,7 +15,7 @@ export function formatDate(date: Date) {
 
 export function readingTime(html: string) {
   const textOnly = html.replace(/<[^>]+>/g, "");
-  const wordCount = textOnly.split(/\s+/).length;
-  const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
+  const charCount = textOnly.length;
+  const readingTimeMinutes = (charCount / 600 + 1).toFixed();
   return `${readingTimeMinutes} 分くらいで読めます`;
 }
